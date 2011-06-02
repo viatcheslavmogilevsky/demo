4613f68ac29f102b3e05d5b68c5ba422f6b3a88a
triggerwoods <keithflint@inbox.ru>
2011-06-02 17:18:59 +0400

    deleted

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
+
 describe PagesController do
   
  render_views
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -27,7 +29,7 @@ describe PagesController do
     end
   end
  
- describe "GET 'about'" do
+  describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
@@ -35,9 +37,21 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | About")
-    end
-  end
+                       :content => "Ruby on Rails Tutorial Sample App | About")
+   end
+ end
 
+  describe "should exist" do
+   it "should be succesful" do
+     get 'help'
+     response.should be_success
+   end
+   it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                       :content => "Ruby on Rails Tutorial Sample App | Help")
+   end
+
+  end
 
 end
