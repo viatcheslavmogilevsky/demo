c89f49a59e60d115ddfd26312e4c820a442d99fb
triggerwoods <keithflint@inbox.ru>
2011-06-04 23:25:30 +0400

    saturday work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
     @title = "Sign up"
   end
   
@@ -7,5 +8,16 @@ class UsersController < ApplicationController
   def show
    @user = User.find(params[:id])
    @title = @user.name
+  end
+ 
+
+  def create
+   @user = User.new(params[:user])
+   if @user.save
+    #sdcasdc
+   else
+    @title = "Sign up"
+    render 'new' 
+   end
   end
 end
