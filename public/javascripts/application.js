1ded333f0bac9a41fc4d8417127d7ad582fc16b9
triggerwoods <keithflint@inbox.ru>
2012-02-17 00:24:37 +0400

    deleted failed migration

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,9 +4,7 @@ var current_table = null;
 window.onload = function(){
   $$('.month_link').each(function(each_link){
     each_link.onclick = function() {
-      if(current_table){ current_table.style.display = 'none';/* Effect.SlideUp(current_table.id, {duration: 3.0});*/ }
-      var x = $('_'+each_link.id);
-      x.style.display = 'block';
+      if(current_table){ current_table.style.display = 'none';
       current_table = x;
     }});
 
